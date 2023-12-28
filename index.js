03b2df2b4efed7a8007b6d8ca4d29de7b0e0701b
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-12-28 20:50:36 +0000

    add questions

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
-const path = require('path');
+const path = require('path'); // optional
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    'What is your GitHub username?',
+    'What is your email address?',
+    'What is the title of the project?',
+    'Please write a short description of your project',
+    'What command should be run to install dependencies?',
+    'What kind of license should your project have?',
+    'What command should be used to run tests?',
+    'What should the user know about using the repo?',
+    'What does the user need to know about contributing to the repo?'
 ];
 
 // function to write README file
